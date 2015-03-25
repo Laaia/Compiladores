c919aa3cc4fb0910a6a81f5a2fe296148fa51e57
Lais <laaia@hotmail.com>
2015-03-25 14:57:45 -0300

    declaração de variáveis

--- Analisador.java
+++ Analisador.java
@@ -10,6 +10,7 @@ class Analisador{
   ArrayList<Tokens> tabelaTokens = new ArrayList<Tokens>();
   ArrayList<Simbolos> tabelaSimbolos = new ArrayList<Simbolos>();
   ArrayList<Posicao> tabelaPosicao = new ArrayList<Posicao>();
+  int indice;
 
   public Analisador(){
     Operadores operador = new Operadores();
@@ -17,6 +18,7 @@ class Analisador{
 
     tabelaOperadores = operador.carregaOperadores(tabelaOperadores);
     tabelaPalavras = palavra.carregaPalavrasReservadas(tabelaPalavras);
+    indice = 59;
   }
 
   public void Analisar(String nomeArquivo){
@@ -47,6 +49,7 @@ class Analisador{
 	}
   public void analisa(String linha, int nLinha, String nArquivo){
     int id = 0;
+    indice++;
     if(linha.matches("^(int(\\s)+|float(\\s)+|double(\\s)+|char(\\s)+)([a-zA-Z])+([0-9])*(\\s)*;$")){
 
       String[] token = linha.split(" ");
@@ -58,33 +61,68 @@ class Analisador{
         }
       }
 
-      tabelaPosicao.add(new Posicao());
-      tabelaPosicao.get(tabelaPosicao.size() - 1).setId(tabelaPosicao.size());
-      tabelaPosicao.get(tabelaPosicao.size() - 1).setLinha(nLinha);
-      tabelaPosicao.get(tabelaPosicao.size() - 1).setColuna(1);
-      tabelaPosicao.get(tabelaPosicao.size() - 1).setArquivo(nArquivo);
+      addPosicao(nLinha, 1, nArquivo);
+      addToken(id, "P. Reservada", tabelaPosicao.size());
 
-      tabelaTokens.add(new Tokens());
-      tabelaTokens.get(tabelaTokens.size() - 1).setId(id);
-      tabelaTokens.get(tabelaTokens.size() - 1).setTipo("Palavra reservada");
-      tabelaTokens.get(tabelaTokens.size() - 1).setPosicao(tabelaPosicao.size());
+      //verificar se a variável não é uma palavra reservada?
+
+      addToken(indice, "Variável", tabelaPosicao.size());
+      addSimbolo(indice, token[1], "Variável", token[0], "Local");
+      addPosicao(nLinha, 2, nArquivo);
+
+      //Buscar id do ';'
+
+      for(int i = 0; i < tabelaOperadores.size(); i++){
+        if(";".equals(tabelaOperadores.get(i).getOperador())){
+          addToken(i, "Operador", tabelaPosicao.size());
+          break;
+        }
+      }
 
       System.out.println(linha);
     }//lascou-se
 
   }
+  public void addSimbolo(int id, String nome, String categoria, String tipo, String escop){
+    tabelaSimbolos.add(new Simbolos());
+    tabelaSimbolos.get(tabelaSimbolos.size() - 1).setId(id);
+    tabelaSimbolos.get(tabelaSimbolos.size() - 1).setNome(nome);
+    tabelaSimbolos.get(tabelaSimbolos.size() - 1).setCategoria(categoria);
+    tabelaSimbolos.get(tabelaSimbolos.size() - 1).setTipo(tipo);
+    tabelaSimbolos.get(tabelaSimbolos.size() - 1).setEscopo(escop);
+  }
+  public void addPosicao(int nLinha, int coluna, String nArquivo){
+    tabelaPosicao.add(new Posicao());
+    tabelaPosicao.get(tabelaPosicao.size() - 1).setId(tabelaPosicao.size());
+    tabelaPosicao.get(tabelaPosicao.size() - 1).setLinha(nLinha);
+    tabelaPosicao.get(tabelaPosicao.size() - 1).setColuna(coluna);
+    tabelaPosicao.get(tabelaPosicao.size() - 1).setArquivo(nArquivo);
+  }
+  public void addToken(int id, String tipo, int posicao){
+    tabelaTokens.add(new Tokens());
+    tabelaTokens.get(tabelaTokens.size() - 1).setId(id);
+    tabelaTokens.get(tabelaTokens.size() - 1).setTipo(tipo);
+    tabelaTokens.get(tabelaTokens.size() - 1).setPosicao(posicao);
+  }
+
 
   public void gerarSaida(){
     System.out.println("\n.::Tabela de Tokens::.");
-    System.out.println("Id         Tipo           Posição");
+    System.out.println("Id\tTipo\t\tPosição");
     for(int i = 0 ; i < tabelaTokens.size(); i++){
-      System.out.println(tabelaTokens.get(i).getId()+ "   "+ tabelaTokens.get(i).getTipo() + "     " + tabelaTokens.get(i).getPosicao());
+      System.out.println(tabelaTokens.get(i).getId()+ "\t"+ tabelaTokens.get(i).getTipo() + "\t" + tabelaTokens.get(i).getPosicao());
     }
 
     System.out.println("\n.::Tabela de Posições::.");
-    System.out.println("Id  Linha   Coluna       Arquivo");
+    System.out.println("Id\tLinha\tColuna\tArquivo");
     for(int i = 0 ; i < tabelaPosicao.size(); i++){
-      System.out.println(tabelaPosicao.get(i).getId()+ "    "+ tabelaPosicao.get(i).getLinha() + "         " + tabelaPosicao.get(i).getColuna()+ "       " + tabelaPosicao.get(i).getArquivo());
+      System.out.println(tabelaPosicao.get(i).getId()+ "\t"+ tabelaPosicao.get(i).getLinha() + "\t" + tabelaPosicao.get(i).getColuna()+ "\t" + tabelaPosicao.get(i).getArquivo());
+    }
+
+    System.out.println("\n.::Tabela de Símbolos::.");
+    System.out.println("Id\tNome\tCategoria\tTipo\tEscopo");
+    for(int i = 0 ; i < tabelaSimbolos.size(); i++){
+      System.out.println(tabelaSimbolos.get(i).getId()+ "\t"+ tabelaSimbolos.get(i).getNome() + "\t" + tabelaSimbolos.get(i).getCategoria()+ "\t" + tabelaSimbolos.get(i).getTipo()+ "\t" + tabelaSimbolos.get(i).getEscopo());
     }
 
   }
